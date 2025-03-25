f2db997f916c1e70d525c6d5af48f79f07d701cf
nelwin5 <99752643+nelwin5@users.noreply.github.com>
2025-03-25 16:12:38 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,17 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "res.cloudinary.com",
+//       },
+//     ],
+//   },
+// };
+
+// export default nextConfig;
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
@@ -8,6 +22,10 @@ const nextConfig: NextConfig = {
         hostname: "res.cloudinary.com",
       },
     ],
+  },
+  eslint: {
+    // Ignore specific ESLint rules during build
+    ignoreDuringBuilds: true,
   },
 };
 
